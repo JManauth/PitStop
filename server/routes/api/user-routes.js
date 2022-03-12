1412440273787e5dc9ac277c73f79a282bbb6bb5
jafet <jafet.manauth@hotmail.com>
2022-03-11 19:58:14 -0600

    got server to connect

---
+++ server/routes/api/user-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const {} = require('../../controllers/user-controller');
+
+const { authMiddleware } =require('../../utils/auth');
+
+module.exports = router;
---
+++ server/utils/auth.js
@@ -0,0 +1,7 @@
+const jwt = require('jsonwebtoken');
+
+//set token secret and expiration date
+const secret ='mysecret';
+const expiration = '2h';
+
+module.exports = {};
