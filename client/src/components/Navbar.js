0992233cbe555b7073ee826a85f6fe4da7dfe152
jafet <jafet.manauth@hotmail.com>
2022-03-13 09:34:07 -0500

    made react app functioning, started on landing page

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import SignUpForm from './SignupForm';
+import LoginForm from './LoginForm';
+
+import Auth from '../utils/auth';
+
+const AppNavbar = () => {
+
+    return(
+        <>
+        <Navbar>
+        </Navbar>
+        </>
+    );
+};
+
+export default AppNavbar;
---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React, {useState}  from "react";
+
+const LandingPage = () => {
+
+    return(
+        <>
+        <h1>WELCOME TO THE PITSTOP</h1>
+        </>
+    );
+};
+
+export default LandingPage;
